a22107f68f14ef501270017fd0be53fdab067b44
sophie12123 <127309133+sophie12123@users.noreply.github.com>
2024-08-27 20:55:55 +1000

    Add files via upload

---
+++ app/components/ProjectCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectCard = ({ imgUrl, title, description }) => {
+  return <div></div>;
+};
+
+export default ProjectCard;
---
+++ app/components/ProjectsSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectsSection = () => {
+  return (
+    <div>
+      <h2>My Project</h2>
+    </div>
+  );
+};
+
+export default ProjectsSection;
